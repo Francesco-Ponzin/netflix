1bcf6676bc40708197f1afb704e67697fc61ab0b
Francesco <francesco.ponzin@gmail.com>
2020-05-06 17:48:02 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { EditGenreComponent } from './edit-genre/edit-genre.component';
 
 
 const routes: Routes = [
-{path: "", redirectTo:"/films", pathMatch: "full"}, //TODO redirect to dashboard
+{path: "", redirectTo:"/dashboard", pathMatch: "full"}, 
 {path: "dashboard", component: DashboardComponent},
 {path: "films", component: FilmsComponent},
 {path: "films/add", component: AddFilmComponent},
