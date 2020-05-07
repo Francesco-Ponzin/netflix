b76f66238ccca8ebaf0c2d01cfa9e9b3fc29768b
Francesco <francesco.ponzin@gmail.com>
2020-05-07 13:06:04 +0200

    filter

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -12,15 +12,24 @@ export class FilmsComponent implements OnInit {
   title = "elenco film";
 
   films: Film[];
+  filteredFilms: Film[];
 
   constructor(public filmService: FilmService) { }
 
   ngOnInit(): void {
     this.filmService.getFilms().subscribe(films => this.films = films);
+    this.filteredFilms = this.films;
   }
 
   edit(film:Film){
     this.filmService.selectedFilm = film;
+  }
+
+  search(event){
+    
+
+   this.filteredFilms =  this.films.filter(x => x.title.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1);
+
   }
 
 
