268054efcb339a75a980382dd8fafcfb297073af
Francesco <francesco.ponzin@gmail.com>
2020-06-06 11:55:46 +0200

    add user based controls in genres

--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GenreService } from '../services/genre.service';
 import { Genre } from '../models/genre';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-genres',
@@ -9,11 +11,14 @@ import { Genre } from '../models/genre';
 })
 export class GenresComponent implements OnInit {
   genres: Genre[];
+  user: User;
 
-  constructor(public genreService: GenreService) { }
+  constructor(private userService: UserService,public genreService: GenreService) { }
 
   ngOnInit(): void {
     this.genreService.getGenres().subscribe(genres => this.genres = genres);
+    this.userService.getLoggedUser().subscribe(user => this.user = user);
+
   }
 
   edit(genre:Genre){
